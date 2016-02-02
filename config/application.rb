2ca7593c813abc294aad419f9b43c9947f3bdcf4
Tanmay Patil <tanmay.ashok.patil@gmail.com>
2016-02-02 00:03:47 -0700

    Added routes for home screen, visualization screen and ajax calls

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
 require File.expand_path('../boot', __FILE__)
-
 require 'rails/all'
+
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -19,6 +19,13 @@ module EventbriteVisuals
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    # Enable the asset pipeline
+    config.assets.enabled = true
+    config.assets.paths << "#{Rails.root}/app/assets/templates"
+    config.assets.paths << "#{Rails.root}/app/assets/fonts"
+    config.assets.precompile += %w(visuals.js loader.gif)
+    config.assets.version = '1.0'
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'home#index'
+  get 'visuals' => 'home#visuals'
+  get 'get_data' => 'home#get_data'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'

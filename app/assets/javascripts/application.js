5f7ef3379b5a11830b0c10ff678d7103ee763ad7
Tanmay Patil <tanmay.ashok.patil@gmail.com>
2016-02-02 00:04:44 -0700

    Added global JS & CSS

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,65 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
-//= require_tree .
+//= require libs/select2
+//= require libs/underscore
+//= require libs/bootstrap
+//= require libs/moment
+//= require libs/daterangepicker
+
+
+
+
+$(document).ready(function(){
+	$('.datepicker-container input[name="daterange"]').daterangepicker({
+		format: 'MM/DD/YYYY',
+		startDate: moment().subtract("month", 3).format("MM/DD/YYYY"),
+		endDate: moment().format("MM/DD/YYYY"),
+		ranges: {
+			'Today': [moment(), moment()],
+			'Yesterday': [moment().subtract('days', 1), moment().subtract('days', 1)],
+			'Last 7 Days': [moment().subtract('days', 6), moment()],
+			'Last 30 Days': [moment().subtract('days', 29), moment()],
+			'This Month': [moment().startOf('month'), moment().endOf('month')],
+			'Last Month': [moment().subtract('month', 1).startOf('month'), moment().subtract('month', 1).endOf('month')]
+		}
+
+	}, function(start, end){
+		// _reports[_reports.type]();
+	});
+
+	$('.button_proceed').click(function(){
+		var query = ""
+		var inputs = $('.home_input');
+		_.each(inputs, function(obj, index){
+			query += $(obj).attr("name") + "=" + $(obj).val();
+			if(index != inputs.length - 1){
+				query += "&";	
+			}
+		});
+		window.open("visuals?" + query, '_self', false)
+	});
+
+});
+
+function getJsonFromUrl() {
+	var query = location.search.substr(1);
+	var result = {};
+	query.split("&").forEach(function(part) {
+	var item = part.split("=");
+	result[item[0]] = decodeURIComponent(item[1]);
+	});
+	return result;
+}
+
+function loader(div){
+	str = '<div class="tc loading_screen_bars">\
+		<div id="dot1" class="loader-dot"></div>\
+		<div id="dot2" class="loader-dot"></div>\
+		<div id="dot3" class="loader-dot"></div>\
+	</div>';
+	if($(div).length > 0){
+		$(div).append(str);
+	}
+	return str;
+};
